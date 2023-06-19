6b377275b2ad0c9b5084f54ecdaa62727f1f6ed1
todocodeacademy <admin@todocodeacademy.com>
2023-06-19 16:54:50 -0300

    ejercicio completo todocode

---
+++ ejerPrompt.js
@@ -0,0 +1,10 @@
+var valor1 = parseInt(window.prompt("Ingrese un número"));
+var valor2 = parseInt(window.prompt("Ingrese otro número"));
+
+//determinar el mayor
+if (valor1>valor2) {
+    alert("El mayor es el valor: " + valor1);
+}
+else {
+    alert("El mayor es el valor: " + valor2);
+}
